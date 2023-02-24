cdcfaae93f5267915c35430f484a08a4562aaaa1
jacksondhall <jacksondhall02@gmail.com>
2023-02-24 13:34:13 -0500

    Started work on drifting

--- Assets/Scripts/JacksonScripts/Drive.cs
+++ Assets/Scripts/JacksonScripts/Drive.cs
@@ -6,11 +6,14 @@ public class Drive : MonoBehaviour
 {
 
     public Rigidbody sphere;
-
     public float forwardAccel = 8f;
     public float reverseAccel = 4f;
     public float maxSpeed = 50f;
-    public float turnStrength = 180;
+    public float turnStrength = 18;
+    public float timer = 0;
+    public float speedTimer = 0;
+    bool isSpeedBoosted = false;
+    bool drifting = false;
     private float speedInput;
     private float turnInput;
 
@@ -24,18 +27,54 @@ public class Drive : MonoBehaviour
     void Update()
     {
         speedInput = 0f;
-        if (Input.GetAxis("Vertical") > 0) {
-            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
-        } 
-        else if (Input.GetAxis("Vertical") < 0) {
-            speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
-        } 
+        if (!Input.GetKey("space")) { 
+            if (Input.GetAxis("Vertical") > 0) {
+                speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+            } 
+            else if (Input.GetAxis("Vertical") < 0) {
+                speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
+            } 
+            turnInput = Input.GetAxis("Horizontal");
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3 (0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
 
-        turnInput = Input.GetAxis("Horizontal");
+            transform.position = sphere.transform.position;
+            drifting = false;
+            if (timer >= 2) {
+                isSpeedBoosted = true;
+            }
+            timer = 0;
+        }
+        else {
+            if (Input.GetAxis("Vertical") > 0) {
+                speedInput = Input.GetAxis("Vertical") * forwardAccel * 500f;
+            } 
+            else if (Input.GetAxis("Vertical") < 0) {
+                speedInput = Input.GetAxis("Vertical") * reverseAccel * 500f;
+            } 
+            turnInput = Input.GetAxis("Horizontal");
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3 (0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3 (0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
-
-        transform.position = sphere.transform.position;
+            transform.position = sphere.transform.position;
+            drifting = true;
+        }
+        if (drifting) {
+            timer += Time.deltaTime;
+            if (timer >= 2) {
+                Debug.Log("Drifting complete");
+            }
+        }
+        if (isSpeedBoosted) {
+            speedTimer += Time.deltaTime;
+            forwardAccel = 32f;
+            maxSpeed = 160f;
+            if (speedTimer >= 1)
+            {
+                forwardAccel = 8f;
+                maxSpeed = 50f;
+                isSpeedBoosted = false;
+                speedTimer = 0;
+            }
+        }
     }
 
     private void FixedUpdate() {
