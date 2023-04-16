50cb36a50fa63473d3b4ef334c3da8b1fb51bac6
Ankhman12 <ankhman12@gmail.com>
2023-04-15 21:41:48 -0400

    Fiddled with Camera some more. Added Drift VFX

--- Assets/Scripts/JacksonScripts/Drive.cs
+++ Assets/Scripts/JacksonScripts/Drive.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Cinemachine;
 public class Drive : MonoBehaviour
 {
     public GameObject minimapIcon;
+    public GameObject meshBody; 
 
     public Rigidbody sphere;
     public float forwardAccel = 8f;
     public float reverseAccel = 4f;
     public float turnStrength = 60f;
     public float driftStrength = 120f;
+    public float turnAngle = 30f;
     public float timer = 0;
     public float speedTimer = 0;
+    public float turnTimer = 0;
+    public float maxTurnTime = 1.5f;
     public float driftTime = 0;
     public bool drifting = false;
     public bool isSpeedBoosted = false;
@@ -49,11 +54,29 @@ public class Drive : MonoBehaviour
     [SerializeField] Animator bikeAnim;
     [SerializeField] Animator racerAnim;
 
+    [Header("Effects")]
+    [SerializeField] ParticleSystem driftSparks_1A;
+    [SerializeField] ParticleSystem driftSparks_2A;
+    [SerializeField] ParticleSystem driftSparks_3A;
+    [SerializeField] ParticleSystem driftSparks_1B;
+    [SerializeField] ParticleSystem driftSparks_2B;
+    [SerializeField] ParticleSystem driftSparks_3B;
+    [SerializeField] ParticleSystem boostFX;
+    [SerializeField] ParticleSystem dustFX;
+
+    [Header("Cameras")]
+    [SerializeField] CinemachineVirtualCamera fwdCam;
+    [SerializeField] CinemachineVirtualCamera bckCam;
+
+
     // Start is called before the first frame update
     void Start()
     {
        sphere.transform.parent = null;
        Instantiate(minimapIcon, transform);
+
+        fwdCam.Priority = 1;
+        bckCam.Priority = 0;
     }
 
     // Update is called once per frame
@@ -72,22 +95,45 @@ public class Drive : MonoBehaviour
         //    stationaryTurning = true;
         //}
         //else if...
-        if (dir > 0) {
+        Debug.Log(dir);
+        if (dir > 0)
+        {
             speedInput = dir * forwardAccel * 1500f;
             //stationaryTurning = false;
-        } 
-        else if (dir < 0) {
+            dustFX.Play();
+
+            //Assign Camera Priority to front
+            fwdCam.Priority = 1;
+            //bckCam.Priority = 0;
+
+        }
+        else if (dir < 0)
+        {
             speedInput = dir * reverseAccel * 1500f;
             drifting = false;
             //stationaryTurning = false;
+            dustFX.Play();
+
+            //Assign Camera Priority to back
+            fwdCam.Priority = 0;
+            //bckCam.Priority = 1;
+        }
+        else if (Input.GetAxis("Forward") == 0 && Input.GetAxis("Backward") == 0)
+        {
+            dustFX.Stop();
         }
 
         turnInput = Input.GetAxis("Horizontal");
 
+        
         //Pass Animation Values
         bikeAnim.SetFloat("BlendX", turnInput);
         bikeAnim.SetFloat("BlendY", dir);
         racerAnim.SetFloat("TurnBlend", turnInput);
+
+        if (Input.GetButtonDown("Drift")) {
+            sphere.AddForce(transform.up * 1000f);
+        }
 
         if (Input.GetAxis("Drift") > 0 && turnInput != 0) {
             driftCheck = true;
@@ -95,6 +141,9 @@ public class Drive : MonoBehaviour
         else {
             driftCheck = false;
         }
+
+        //Calculate Angular Drag based on current velocity
+        float angularDrag = Mathf.Min(10 / (sphere.GetPointVelocity(sphere.position).magnitude + 1), .7f);
 
         if (driftCheck && !drifting && turnInput != 0) {
             drifting = true;
@@ -106,14 +155,12 @@ public class Drive : MonoBehaviour
             driftCheck = false;
         } 
         else { //Normal Turning
-            //Calculate Angular Drag based on current velocity
-            float angularDrag = Mathf.Min(10 / (sphere.GetPointVelocity(sphere.position).magnitude + 1), .7f);
-            Debug.Log(angularDrag);
-
+            
+            //Debug.Log(angularDrag);
 
             if (dir > 0)
             {
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + transform.up * (turnInput * turnStrength * Time.deltaTime * 2f * angularDrag));
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + transform.up * (turnInput * turnStrength * Time.deltaTime * 3f * angularDrag));
             }
             else if (dir == 0 && Input.GetAxis("Forward") > 0 && Input.GetAxis("Backward") > 0) //Turn slowly when using both forward and reverse input, don't apply angular drag
             {
@@ -128,17 +175,48 @@ public class Drive : MonoBehaviour
             transform.position = sphere.transform.position;
         }
 
+        //Turn Timer & Tilting
+        float rotStep = 20f * Time.deltaTime;
+        if (turnInput != 0 && turnTimer < maxTurnTime)
+        {
+            turnTimer += Time.deltaTime;
+            Debug.Log(turnTimer);
+            float driftMultiplier = driftCheck ? 3f : 1f;
+            if (turnInput > 0) //&& isBike
+            {
+                meshBody.transform.localRotation = Quaternion.RotateTowards(meshBody.transform.localRotation, Quaternion.Euler(0, 0, -30), driftMultiplier * rotStep);
+            }
+            else if (turnInput < 0) //&& isBike
+            {
+                meshBody.transform.localRotation = Quaternion.RotateTowards(meshBody.transform.localRotation, Quaternion.Euler(0, 0, 30), driftMultiplier * rotStep);
+            }
+        }
+        else if (turnInput == 0) //Tilt bike when not turning to be upright
+        {
+            turnTimer = 0;
+            meshBody.transform.localRotation = Quaternion.RotateTowards(meshBody.transform.localRotation, Quaternion.Euler(0, 0, 0), 4f * rotStep);
+        }
+
         //Stationary Turning
         //if (stationaryTurning && turnInput != 0) {
         //    transform.Rotate(0, 2f * Time.deltaTime * turnInput * turnStrength, 0, Space.Self);
         //    transform.position = sphere.transform.position;
         //}
 
-        if (drifting) {
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + transform.up * (driftDirection * driftStrength * Time.deltaTime * .5f));
+        if (drifting)
+        {
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + transform.up * (driftDirection * driftStrength * Time.deltaTime * 3f * angularDrag));
             transform.position = sphere.transform.position;
             driftTime = driftTime + Time.deltaTime;
-            if (driftTime > 3) {
+            if (driftTime > 1.8f)
+            {
+                if (!driftSparks_3B.isPlaying)
+                {
+                    driftSparks_2B.Stop();
+                    driftSparks_3A.Play();
+                    driftSparks_3B.Play();
+                }
                 Debug.Log("Drift level 3");
                 if (driftClickSource.clip != driftClick3)
                 {
@@ -146,21 +224,51 @@ public class Drive : MonoBehaviour
                     driftClickSource.Play();
                 }
             }
-            else if (driftTime > 2) {
+            else if (driftTime > 1.2f)
+            {
+                //Update Effects
+                if (!driftSparks_2B.isPlaying)
+                {
+                    driftSparks_1B.Stop();
+                    driftSparks_2A.Play();
+                    driftSparks_2B.Play();
+                }
                 Debug.Log("Drift level 2");
-                if(driftClickSource.clip != driftClick2)
+                if (driftClickSource.clip != driftClick2)
                 {
                     driftClickSource.clip = driftClick2;
                     driftClickSource.Play();
                 }
             }
-            else if (driftTime > 1) {
+            else if (driftTime > .6f)
+            {
+                //Update Effects
+                if (!driftSparks_1B.isPlaying)
+                {
+                    driftSparks_1A.Play();
+                    driftSparks_1B.Play();
+                }
                 Debug.Log("Drift level 1");
                 if (driftClickSource.clip != driftClick1)
                 {
                     driftClickSource.clip = driftClick1;
                     driftClickSource.Play();
                 }
+            }
+        }
+        else {
+            //Stop Drift Effects when not drifting
+            if (driftSparks_1B.isPlaying)
+            {
+                driftSparks_1B.Stop();
+            }
+            if (driftSparks_2B.isPlaying)
+            {
+                driftSparks_2B.Stop();
+            }
+            if (driftSparks_3B.isPlaying)
+            {
+                driftSparks_3B.Stop();
             }
         }
 
@@ -187,18 +295,21 @@ public class Drive : MonoBehaviour
             boostTime = 3;
             driftBoostSource.clip = driftBoost3;
             driftBoostSource.Play();
+            boostFX.Play();
         }
         else if (driftTime > 2) {
             isSpeedBoosted = true;
             boostTime = 2;
             driftBoostSource.clip = driftBoost2;
             driftBoostSource.Play();
+            boostFX.Play();
         }
         else if (driftTime > 1) {
             isSpeedBoosted = true;
             boostTime = 1;
             driftBoostSource.clip = driftBoost1;
             driftBoostSource.Play();
+            boostFX.Play();
         }
         driftTime = 0;
         driftClickSource.clip = null;
